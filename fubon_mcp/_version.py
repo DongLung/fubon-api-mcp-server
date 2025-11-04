2ed1d81d8b62ef561d5f059e55db46f945d74edd
Jimmy.Cui <iron0714@hotmail.com>
2025-11-04 17:18:07 +0800

    fix: format _version.py imports to match black/isort profile

--- fubon_mcp/_version.py
+++ fubon_mcp/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '1.8.0.post3+gd134c3d29.d20251104'
-__version_tuple__ = version_tuple = (1, 8, 0, 'post3', 'gd134c3d29.d20251104')
+__version__ = version = '1.8.0.post4+g2fb9fb0a5.d20251104'
+__version_tuple__ = version_tuple = (1, 8, 0, 'post4', 'g2fb9fb0a5.d20251104')
 
-__commit_id__ = commit_id = 'gd134c3d29'
+__commit_id__ = commit_id = 'g2fb9fb0a5'
